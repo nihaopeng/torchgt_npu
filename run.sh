948b021234204c67f39c9e85c835bc08a4bfc357
nihaopeng <YutaoPeng@foxmail.com>
2025-11-02 19:13:00 +0800

    build essential

---
+++ run.sh
@@ -0,0 +1,2 @@
+# TorchGT
+torchrun --nproc_per_node=2 --master_port 8081 main_sp_node_level.py --dataset ogbn-arxiv --seq_len 64000 --n_layers 4 --hidden_dim 64 --ffn_dim 64 --num_heads 8 --epochs 2000 --model gt --attn_type sparse --reorder 
